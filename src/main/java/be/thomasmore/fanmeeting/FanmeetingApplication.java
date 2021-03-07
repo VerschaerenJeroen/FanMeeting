12d5a78011c1ebc6ddfdcbe32a2c4ddb8bb7eba1
VerschaerenJeroen <verschaerenjeroen@gmail.com>
2021-03-07 14:11:02 +0100

    First project Setup

---
+++ src/main/java/be/thomasmore/fanmeeting/FanmeetingApplication.java
@@ -0,0 +1,13 @@
+package be.thomasmore.fanmeeting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FanmeetingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FanmeetingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/be/thomasmore/fanmeeting/FanmeetingApplicationTests.java
@@ -0,0 +1,13 @@
+package be.thomasmore.fanmeeting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FanmeetingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
