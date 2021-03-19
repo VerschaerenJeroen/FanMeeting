2ac0122038d2a5be4fb7f63209a037e7a8c2f61f
VerschaerenJeroen <verschaerenjeroen@gmail.com>
2021-03-19 22:47:48 +0100

    Made championlist.html + bootstrap

--- src/main/java/be/thomasmore/meeting/controllers/ChampionController.java
+++ src/main/java/be/thomasmore/meeting/controllers/ChampionController.java
@@ -3,25 +3,44 @@ package be.thomasmore.meeting.controllers;
 
 import be.thomasmore.meeting.model.Champion;
 import be.thomasmore.meeting.repositories.ChampionRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
 @Controller
 public class ChampionController {
+    private Logger logger = LoggerFactory.getLogger(ChampionRepository.class);
 
     @Autowired
     private ChampionRepository championRepository;
 
-    @GetMapping({"/championlist"})
-    public String championList(Model model) {
-        Iterable<Champion> champions;
+    @GetMapping("/championlist")
+    public String championList(Model model,
+                               @RequestParam(required = false) String keyword,
+                               @RequestParam(required = false) String filterChampions) {
 
-        champions = championRepository.findAll();
+        logger.info(String.format("championList -- filterchampions=%s", filterChampions));
+
+        List<Champion> champions;
+        if (keyword != null && filterChampions != null) {
+            champions = championRepository.findByFilterQuery(keyword, filterChampions);
+        }
+        else {
+            champions = championRepository.findAllBy();
+        }
+
         model.addAttribute("champions", champions);
+        model.addAttribute("nrOfChampions", champions.size());
+        model.addAttribute("keyword", keyword);
+        model.addAttribute("search", champions.isEmpty());
+        model.addAttribute("filterChampions", filterChampions);
+
         return "championlist";
     }
 }
--- src/main/java/be/thomasmore/meeting/repositories/ChampionRepository.java
+++ src/main/java/be/thomasmore/meeting/repositories/ChampionRepository.java
@@ -1,7 +1,16 @@
 package be.thomasmore.meeting.repositories;
 
 import be.thomasmore.meeting.model.Champion;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface ChampionRepository extends CrudRepository<Champion, Integer> {
+    List<Champion> findAllBy();
+
+    @Query("select c from Champion c where " +
+            "(?1 is null or upper(c.championName) like upper(concat('%',?1,'%'))) and " +
+            "(?2 is null or upper(?2) = upper(c.role) or ?2 = 'all')")
+    List<Champion> findByFilterQuery(String name, String role);
 }
